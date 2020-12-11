83a56ee57add067cec89953ffab20854d8ea32b5
getuser78 <75839935+getuser78@users.noreply.github.com>
2020-12-11 12:55:21 +0100

    Update fled.php

--- fled.php
+++ fled.php
@@ -1,6 +1,6 @@
 {"GlobalCanliTvPro":[
 {"ads_tip":"Admob",
-"main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
+"main_link":"https://github.com/getuser09/file/raw/master/app/pays.php",
 "app_id":"ca-app-pub-7224510510769089~8458725743",
 "banner_unit_id":"ca-app-pub-7224510510769089/7145644079",
 "inter_unit_id":"ca-app-pub-7224510510769089/3228442187",
