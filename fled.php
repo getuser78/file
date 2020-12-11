7ed54b14eea30501cb85cf621452ce1c672a05d8
getuser78 <75839935+getuser78@users.noreply.github.com>
2020-12-11 12:46:07 +0100

    Create fled.php

---
+++ fled.php
@@ -0,0 +1,8 @@
+{"GlobalCanliTvPro":[
+{"ads_tip":"Admob",
+"main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
+"app_id":"ca-app-pub-7224510510769089~8458725743",
+"banner_unit_id":"ca-app-pub-7224510510769089/7145644079",
+"inter_unit_id":"ca-app-pub-7224510510769089/3228442187",
+"iron_id":""},
+]}
