c45972c03069efd6469c2c2bcf6f4b1d6f3c2c44
getuser78 <75839935+getuser78@users.noreply.github.com>
2020-12-19 02:34:22 +0100

    cc

--- fled.php
+++ fled.php
@@ -2,7 +2,7 @@
 {"ads_tip":"AdColony",
 "main_link":"https://github.com/getuser09/file/raw/master/app/pays.php",
 "app_id":"app17c1c0c11be04dab90",
-"banner_unit_id":"583004352309411_583254492284397",
+"banner_unit_id":"vz1ae295f4bfb4439395",
 "inter_unit_id":"vz2c27d86da48c48eeb1",
 "iron_id":""},
 ]}
