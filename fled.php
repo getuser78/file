6667ab5905101fb7869f983e73789d11edb25f89
getuser78 <75839935+getuser78@users.noreply.github.com>
2020-12-19 02:26:53 +0100

    cc

--- fled.php
+++ fled.php
@@ -1,8 +1,8 @@
 {"GlobalCanliTvPro":[
-{"ads_tip":"Facebook",
+{"ads_tip":"AdColony",
 "main_link":"https://github.com/getuser09/file/raw/master/app/pays.php",
-"app_id":"240555393761530",
+"app_id":"app17c1c0c11be04dab90",
 "banner_unit_id":"583004352309411_583254492284397",
-"inter_unit_id":"583004352309411_583261545617025",
+"inter_unit_id":"vz2c27d86da48c48eeb1",
 "iron_id":""},
 ]}
