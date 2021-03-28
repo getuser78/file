8504ca9d2b74c37a258ffe5134f043ee096d307b
getuser78 <75839935+getuser78@users.noreply.github.com>
2021-03-28 01:20:55 +0100

    cc

--- fleed.php
+++ fleed.php
@@ -1,5 +1,5 @@
 {"GlobalCanliTvPro":[
-{"ads_tip":"Admob",
+{"ads_tip":"Unity",
 "main_link":"https://github.com/getuser09/file/raw/master/app/ch/morocco/pays.php",
 "app_id":"ca-app-pub-6793099181498904~4195128208",
 "banner_unit_id":"ca-app-pub-6793099181498904/4901721123",
